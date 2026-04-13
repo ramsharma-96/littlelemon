8241c72f67c2548aaa76d3a14b76af4749cdad8e
Ram Sharma <thespykar2020@gmail.com>
2026-04-13 14:52:01 +0530

    models created for booking and menuitem

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Booking, MenuItem
+
 # Register your models here.
+
+admin.site.register(MenuItem)
+admin.site.register(Booking)
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
+    
+    def __str__(self):
+        return f'{self.title}:{self.price}'
+    
+
+
+class Booking(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255, default=None)
+    no_of_guests = models.IntegerField(default=0)
+    booking_date = models.DateField(auto_now=False, default=None)
+
+    def get_item(self):
+        return f'{self.name}-{self.no_of_guests}-{self.booking_date}'
+    
+    def __str__(self):
+        return f'{self.name}-{self.no_of_guests}-{self.booking_date}'
