87ae63f63bb04d7b2c2c9e1c366358923b7dfb2c
Ram Sharma <thespykar2020@gmail.com>
2026-04-13 14:01:13 +0530

    static files loaded in the index.html file

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,13 @@
+from urllib import request
+
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def sayHello(request):
+ return HttpResponse('Hello World')
+
+
+def index(request):
+    return render(request, 'index.html', {})
